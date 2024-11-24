51bf46d9b7ec4e138fda2b2ee85ccdd45c57ac33
Alexander Novikov <nvkv_ai@outlook.com>
2024-11-24 14:48:41 +0300

    refactor

--- HelloWorld/Script.fsx
+++ HelloWorld/Script.fsx
@@ -1,7 +1,17 @@
-System.Math.Sqrt
+let days_in_month = function
+    | 1 -> 31 // January
+    | 2 -> 28 // February (non-leap year)
+    | 3 -> 31 // March
+    | 4 -> 30 // April
+    | 5 -> 31 // May
+    | 6 -> 30 // June
+    | 7 -> 31 // July
+    | 8 -> 31 // August
+    | 9 -> 30 // September
+    | 10 -> 31 // October
+    | 11 -> 30 // November
+    | 12 -> 31 // December
+    | _ -> 0 // Invalid month
 
-let g(n) = n + 5 // int
-
-let gg = fun n -> n + 5
-
-let h (x,y) = sqrt (float x*x + float y*y)
+let v = days_in_month(13)
+printfn "%d" v
