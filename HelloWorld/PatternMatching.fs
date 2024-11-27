747aa21d5bda7672bf0bd6e1e0743f48b6ca2e4c
Alexander Novikov <nvkv_ai@outlook.com>
2024-11-27 07:54:17 +0300

    refactor

--- HelloWorld/PatternMatching.fs
+++ HelloWorld/PatternMatching.fs
@@ -1,14 +1,5 @@
 let days_in_month = function
-    | 1 -> 31 // January
+    | 1 | 3 | 5 | 7 | 8 | 10| 12 -> 31 
     | 2 -> 28 // February (non-leap year)
-    | 3 -> 31 // March
-    | 4 -> 30 // April
-    | 5 -> 31 // May
-    | 6 -> 30 // June
-    | 7 -> 31 // July
-    | 8 -> 31 // August
-    | 9 -> 30 // September
-    | 10 -> 31 // October
-    | 11 -> 30 // November
-    | 12 -> 31 // December
+    | 4 | 6 | 9 | 11 -> 30
     | _ -> 0 // Invalid month
