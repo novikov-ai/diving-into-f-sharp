889aa6d3dd30a3b9c04bfe05746126487db7c808
Alexander Novikov <nvkv_ai@outlook.com>
2024-12-22 19:19:02 +0300

    update

--- HelloWorld/List.fs
+++ HelloWorld/List.fs
@@ -1,9 +1,12 @@
 // 34.1
-let rec upto n = [1..n]
+let rec upto = function
+  | 0 -> []
+  | n -> upto(n-1) @ [n]
 
 // 34.2
-let rec dnto n = 
-  if n <= 0 then [] else n::dnto(n-1)
+let rec dnto = function 
+  | 0 -> []
+  | n -> n::dnto(n-1)
 
 // 34.3
 let rec evenn = function
